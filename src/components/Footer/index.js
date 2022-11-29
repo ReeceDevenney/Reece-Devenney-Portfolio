5dbc4f2e9e172a9c32a2503023d3e9b4690b70aa
Reece Devenney <reecedevenney@gmail.com>
2022-11-28 23:35:13 -0500

    changed footer image spacing

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,9 @@ import React from "react";
 function Footer() {
     return(
         <footer>
-            <a href="https://github.com/ReeceDevenney" target="_blank" className="footerImg"><img src="https://via.placeholder.com/80"></img></a>
-            <a href="https://www.linkedin.com/in/reece-d-83753520a/" target="_blank" className="footerImg"><img src="https://via.placeholder.com/80"></img></a>
-            <a href="#" target="_blank" className="footerImg"><img src="https://via.placeholder.com/80"></img></a>
+            <a href="https://github.com/ReeceDevenney" target="_blank" className="footerImg"><img src="https://via.placeholder.com/45"></img></a>
+            <a href="https://www.linkedin.com/in/reece-d-83753520a/" target="_blank" className="footerImg"><img src="https://via.placeholder.com/45"></img></a>
+            <a href="#" target="_blank" className="footerImg"><img src="https://via.placeholder.com/45"></img></a>
         </footer>
     )
 }
