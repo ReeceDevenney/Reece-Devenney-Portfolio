9895a5897f2b5595fa6afcdeb83e3152857eda33
Reece Devenney <reecedevenney@gmail.com>
2022-11-29 12:27:33 -0500

    added icons to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { ReactComponent as Github } from '../../img/icons/github.svg'
+import { ReactComponent as Linkedin } from '../../img/icons/linkedin.svg'
+import { ReactComponent as Stackoverflow } from '../../img/icons/stackoverflow.svg'
+
 
 function Footer() {
     return(
         <footer>
-            <a href="https://github.com/ReeceDevenney" target="_blank" className="footerImg"><img src="https://via.placeholder.com/45"></img></a>
-            <a href="https://www.linkedin.com/in/reece-d-83753520a/" target="_blank" className="footerImg"><img src="https://via.placeholder.com/45"></img></a>
-            <a href="#" target="_blank" className="footerImg"><img src="https://via.placeholder.com/45"></img></a>
+            <a href="https://github.com/ReeceDevenney" target="_blank" className="footerImg"><Github /></a>
+            <a href="https://www.linkedin.com/in/reece-d-83753520a/" target="_blank" className="footerImg"><Linkedin /></a>
+            <a href="#" target="_blank" className="footerImg"><Stackoverflow /></a>
         </footer>
     )
 }
