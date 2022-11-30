ce7d5708943aff0558cc7689649a3ce4b064fe77
Reece Devenney <reecedevenney@gmail.com>
2022-11-29 19:52:58 -0500

    added contact form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Footer from './components/Footer';
 import Mywork from './components/Mywork';
+import Contact from './components/Contact';
 
 function App() {
   const [sections] = useState([
@@ -25,6 +26,7 @@ const [currentSection, setCurrentSection] = useState(sections[0])
       <main>
         {currentSection === sections[0] && <About></About>} 
         {currentSection === sections[1] && <Mywork></Mywork>}
+        {currentSection === sections[3] && <Contact></Contact>}
       </main>
       <Footer></Footer>
     </div>
---
+++ src/components/Contact/index.js
@@ -0,0 +1,63 @@
+import React, {useState} from 'react'
+
+
+function Contact() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' })
+    const { name, email, message } = formState;
+
+    const [errorMessage, setErrorMessage] = useState('');
+    
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+      }
+
+    function handleChange(e) {
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+    }
+
+    return (
+        <section id='contact'>
+            <h1 id='contact-title'>Contact me</h1>
+            <form id="contact-form">
+                <div>
+                    <label htmlFor="name"></label>
+                    <input type="text" name="name" defaultValue={name} onBlur={handleChange} placeholder='Name'/>
+                </div>
+                <div>
+                    <label htmlFor="email"></label>
+                    <input type="email" name="email" defaultValue={email} onBlur={handleChange} placeholder='email: John.Doe@gmail.com'/>
+                </div>
+                <div>
+                    <label htmlFor="message"></label>
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} placeholder='Your message here'/>
+                </div>
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                <button type="submit">Submit</button>
+            </form>
+        </section>
+    )
+}
+
+export default Contact
