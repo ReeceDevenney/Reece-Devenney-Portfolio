4f1db8b4149a7ae36087eda6eb92d4b9bab0a70c
reece <reecedevenney@gmail.com>
2023-08-25 10:58:44 -0400

    updated with new project

--- src/components/Mywork/index.js
+++ src/components/Mywork/index.js
@@ -3,20 +3,27 @@ import React, { useState } from "react";
 function Mywork() {
     const [projects] = useState([
         {
+            name: 'Shop-It',
+            description: 'An ecomerce site where users can post and buy products from others',
+            live: 'https://shop-it-ac92e.web.app/',
+            github: 'https://github.com/ReeceDevenney/shop-it',
+            tech: "Typescript, Vue, Firebase"
+        },
+        {
             name: 'Arij',
             description: 'A task managment site for teams to creat and track issue tickets',
             live: 'https://pacific-cliffs-88620.herokuapp.com/',
             github: 'https://github.com/Jstelzl/arij',
-            tech: "Javascript React GraphQL"
+            tech: "Javascript, React, GraphQL"
         },
         {
             name: 'PokeNect',
             description: 'A social media site for fans of Pokemon to find and follow other enthusiest, and discuss the game',
             live: 'https://dry-wildwood-44635.herokuapp.com/',
             github: 'https://github.com/PDismore/PokeNect',
-            tech: "Javascript SQL"
+            tech: "Javascript, SQL"
         }
-        
+
     ])
     return (
         <div>
@@ -27,8 +34,8 @@ function Mywork() {
                         <p>{project.description}</p>
                         <p className="techUsed">{project.tech}</p>
                         <div>
-                        <a href={project.github} target='_blank' rel="noreferrer"> Github Repo | </a>
-                        <a href={project.live} target='_blank' rel="noreferrer"> Link </a>
+                            <a href={project.github} target='_blank' rel="noreferrer"> Github Repo | </a>
+                            <a href={project.live} target='_blank' rel="noreferrer"> Link </a>
                         </div>
                     </div>
                 </div>
